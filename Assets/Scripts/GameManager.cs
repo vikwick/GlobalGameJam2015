a0e1748e1ff0d7d978f31a2f50d27c916d202957
Slagernicus <crisluiskroeze@yahoo.com>
2015-01-24 16:41:42 -0500

    commiting stuff 4:41

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    GameObject player;
+    //GameObject enemy;
+    //EnemyController _enemy;
+    //GameObject door;
+    OGChickenController _player;
+    //anim = GetComponent<Animator>();
+	// Use this for initialization
+	void Start () {
+	    player = GameObject.FindGameObjectWithTag("Player");
+        _player = player.GetComponent<OGChickenController>();
+        //enemy = GameObject.FindGameObjectWithTag("Enemy");
+        //door = GameObject.FindGameObjectWithTag("Door");
+
+   }
+	
+	// Update is called once per frame
+	void Update () {
+	    if ((int)_player.currentHP == 0 || (int)_player.infectedLevel == 10){
+            Destroy(player);
+        }
+        //if ((int)_enemy.enemyHP == 0){
+            //Destroy (enemy);
+        //}
+        //if ((int)_enemy.currentEnemyCount == 0){
+            //Destroy(door);
+            //anim.SetBool(enemiesClear, true);
+        //}
+	}
+}
--- Assets/Scripts/OGChickenController.cs
+++ Assets/Scripts/OGChickenController.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using System;
 
 public class OGChickenController : MonoBehaviour {
-    bool facingLeft = true;
     public float maxSpeed = 10f;
     public float HP = 100f;
     public float infectedLevel = 0f;
     public string ability = "hello";
     public float ATK = 5f;
-
+    public float currentHP = 100f;
+    private float idleTime = 0f;
+    bool timesUp = false;
     Animator anim;
     // Use this for initialization
     void Start () {
@@ -26,19 +27,46 @@ public class OGChickenController : MonoBehaviour {
         anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
 
         rigidbody2D.velocity = new Vector2 ((maxSpeed * Input.GetAxis("Horizontal")), maxSpeed * Input.GetAxis("Vertical"));
-        anim.SetFloat("Speed", Math.Abs(rigidbody2D.velocity.x));
-        
-        if (rigidbody2D.velocity.x < 0.0 && !facingLeft){
-            Flip();
-            }
-        else if (rigidbody2D.velocity.x > 0.0 && facingLeft){
-            Flip();
-            }
+        anim.SetFloat("Speed", rigidbody2D.velocity.x);
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle")) {
+            idleTime+=Time.deltaTime;
+        }
+        else{
+            idleTime = 0;
+        }
+        if((int)idleTime%3==0 && !timesUp){
+            timesUp = true;
+        }
+        else if(((int)idleTime+1)%3==0 & timesUp){
+            timesUp = false;
+            infectedLevel += 5;
+        }
+        if(Input.GetButtonDown("attack")){
+            anim.SetTrigger("attacking");
+        }
+
     }
-    void Flip () {
-        Vector3 theScale = transform.localScale;
-        theScale.x = theScale.x * -1;
-        transform.localScale = theScale;
-        facingLeft = !facingLeft;
+
+    void OnGUI(){
+        float boxSizeInfected = (Screen.width / 2) / (100/infectedLevel);
+        float boxSizeHP = Screen.width / 2 / (HP/(currentHP+.1f));
+        GUI.color = Color.black;
+        GUI.Box(new Rect(10, 10, Screen.width / 2, 20), "HP:" + currentHP + "/" + HP);
+        GUI.Box(new Rect(10, 30, Screen.width / 2, 20), "Infection:" + infectedLevel + "%");
+        if ((int)currentHP > 3){
+            GUI.color = Color.green;
+            GUI.Button(new Rect(10, 10, boxSizeHP, 20), "");
+        }
+        if ((int)infectedLevel > 3){
+            GUI.color = Color.magenta;
+            GUI.Button(new Rect (10, 30, boxSizeInfected, 20), "");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D c){
+        Debug.Log(c.gameObject.tag);
+        if (c.gameObject.tag == "Enemy" && !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack")){
+            currentHP -= 10f;
+        }
     }
 }
