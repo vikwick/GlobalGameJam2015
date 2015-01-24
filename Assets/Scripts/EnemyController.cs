2f6f5aad61e24de8871073a122186b445d84c875
modestvillain <tyler.d.stowell@gmail.com>
2015-01-24 03:06:30 -0500

    please work

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public int ATK;
+	public int HP;
+	public int SPD;
+	public int RNG;
+	GameObject p;
+
+	// Use this for initialization
+	void Start () {
+		p = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollision2DEnter(Collision2D c)
+	{
+		if(c.gameObject.tag == "Player")
+		{
+			// do damage
+		}
+	}
+
+	void kill()
+	{
+		Vector2 pos = p.transform.position;
+		rigidbody2D.AddForce((pos - transform.position)*SPD);
+		if(pos.magnitude < RNG) // player in range
+		{
+			attack();
+		}
+	}
+
+	void attack()
+	{
+		// set attack bool to change animation
+
+	}
+}
